3b3b48fe233ad516650d51c222842e5dc56a60f3
Jared Upchurch <reeeegaaaal@gmail.com>
2026-04-06 20:11:13 -0500

    saving progress

--- src/story/dialogue/generic_dialogue.py
+++ src/story/dialogue/generic_dialogue.py
@@ -16,29 +16,103 @@ from src.mechanics.crew import Crew, role_vocabularies, archetype_vocabularies
 # % = EOS will invoke a yes/no prompt
 ## -----------------------------------------------##
 
+nouns = [
+    "nn_area",
+    "nn_boat",
+    "nn_chaos",
+    "nn_customer", #patron guest trader
+    "nn_dark",
+    "nn_end",
+    "nn_hate",
+    "nn_help",
+    "nn_items", #goods wares supplies
+    "nn_light",
+    "nn_love",
+    "nn_money",
+    "nn_order",
+    "nn_pain",
+    "nn_power",
+    "nn_say",
+    "nn_show",
+    "nn_start",
+    "nn_time",
+    "nn_truth"
+]
+
+adjectives = [
+    "aa_dark",
+    "aa_bright",
+    "aa_empty",
+    "aa_full",
+    "aa_old",
+    "aa_young",
+    "aa_sad",
+    "aa_safe",
+    "aa_calm",
+    "aa_new", #fresh 
+]
+
+verbs = [
+    "vv_end", #finish 
+    "vv_empty", # deplete
+    "vc_get", # cognitive -  get, understand, follow, etc
+    "vt_get", # transitive - acquire, obtain, fetch, etc
+    "vv_need", # require demand want long-for 
+    "vv_help", #assist 
+    "vv_light",
+    "vv_love",
+    "vv_make",
+    "vv_order",
+    "vv_return",
+    "vv_say",
+    "vv_see", # look-at
+    "vv_show",
+    "vv_start",
+    "vv_think",
+    "vv_time",
+    "vv_trade",
+    "vv_travel",
+    "vv_use",
+    "vv_want", # modal - want, desire, yearn, etc
+    "vv_have", # shop context: carry, sell, trade
+]
+
+discourse = [
+    "dd_hello",
+    "dd_goodbye",
+    "dd_bye",
+    "dd_well", # um, er, eh, so, etc
+    "dd_yes",
+    "dd_no",
+    "dd_thanks",
+    "dd_off", # away begone depart
+    "dd_you", 
+    "dd_please", 
+]
+
 general_dialog_trees = {
     'shop': {
         'new':{
             'initiate':[
                 [
-                    'Hello, can I interest you in any of my goods?',
-                    "Yes, lets look at what you have for sale.*",
-                    "Travel safe, come back again sometime."],
+                    'dd_hello vv_want to vv_see my nn_items?',
+                    "dd_yes, lets vv_trade.*",
+                    "vv_travel aa_safe, vv_return sometime."],
                 [
-                    "You there, come see my goods!",
-                    "OK. . . *that was abrupt**",
-                    "Thanks, now off with you. I have other customers waiting!&"    
+                    "dd_hello vv_see my nn_items!",
+                    "dd_well . . . *that was abrupt*",
+                    "dd_thanks, now dd_off with you. I have another nn_customer waiting!"    
                 ]
             ],
             'respond':[
                 [
-                    "Hello, we are new in the area. Do you have goods for sale?",
-                    "Hello! Always glad to see a new face in the area. Yes, look at my goods.*"
-                    "We'll come by again if we need other goods."
+                    "dd_hello we are aa_new in the nn_area. Do you have nn_items to vv_trade?",
+                    "dd_hello Always glad to see a aa_new face in the nn_area. dd_yes, vv_see my nn_items.*"
+                    "We'll come by again if we vv_want other nn_items."
                 ],
                 [
                     "Good =. *cough cough* Do you have anything for allergies? I'm not used to the air here.",
-                    "Not sure about allergies, but you can take a look at my goods. . . *",
+                    "Not sure about allergies, but you can vv_see my nn_items. . . *",
                     "Well, i'll keep looking just in case."
                 ]
             ]
@@ -153,7 +227,7 @@ general_dialog_trees = {
         'new':{
             'initiate':[
                 [
-                    "Hey, are you new here? This place doesn't usually take kindly to strangers.",
+                    "dd_hello, are you aa_new here? This place doesn't usually take kindly to strangers.",
                     "We are. Thanks for the advice, we will tread carefully."
                 ],
                 [
